857d1366adcc3d729d8e4ed4fcab6ef806c04d75
wu7 <wu7czz@gmail.com>
2019-04-03 15:39:19 +0800

    连接前端测试调试

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ import Koa from 'koa'
 import KoaStatic from 'koa-static'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
+// import fs from 'fs'
+// import enforceHttps from 'koa-sslify'
+// import https from 'https'
 import config from './config'
 import {database} from './mongodb'
 
 database()
 
+// const options = {
+//    key: fs.readFileSync('./ssl/wu7.key'),  //ssl文件路径
+//    cert: fs.readFileSync('./ssl/wu7.pem')  //ssl文件路径
+// }
 const GraphqlRouter = require('./router')
 
 const app = new Koa()
 const router = new Router()
-
 const port = config.port || 4000
 
+// app.use(enforceHttps()) 
 app.use(bodyParser());
 app.use(KoaStatic(__dirname + '/public'));
 
@@ -23,4 +30,5 @@ app.use(router.routes())
    .use(router.allowedMethods());
 
 app.listen(port);
+
 console.log('ele server listen port: ' + port)
--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000, // 项目启动的端口
+  port: 3030, // 项目启动的端口
   dbPath: 'mongodb://localhost:27017/ele', // 数据库
   saltTimes: 3 // 加盐的次数（用户密码加密）
 }
